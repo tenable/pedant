652c2d10edef0e096c84f596d9d5573f5796c596
Mak Kolybabi <mak@kolybabi.com>
2011-12-27 19:21:54 -0600

    Add test for registration section checking.

--- lib/pedant/checks/contains_registration_section.rb
+++ lib/pedant/checks/contains_registration_section.rb
@@ -40,53 +40,66 @@ module Pedant
       tree = @kb[:trees][@kb[:main]]
 
       # Find the registration If statement.
-      reg = []
-      tree.all(:If).each do |node|
-        next if !node.cond.is_a? Nasl::Lvalue
-        next if node.cond.ident.name != 'description'
-        reg << node
+      regs = tree.all(:If).select do |node|
+        (node.cond.is_a?(Nasl::Lvalue) && node.cond.ident.name == 'description')
       end
 
       # Ensure there's a registration section.
-      if reg.empty?
+      if regs.empty?
         report(:error, "No registration section was found.")
         report(:error, "This will cause the plugin to be run twice in both Nessus interface and nasl with the -M flag.")
         return fail
       end
 
       # Ensure that there is only one registration section.
-      if reg.length != 1
-        report(:error, "#{reg.length} registration sections were found.")
+      unless regs.length == 1
+        report(:error, "Multiple registration sections were found.")
+        regs.each { |reg| report(:error, reg.context) }
         return fail
       end
 
       # Ensure that the registration section is a block.
-      reg = reg.first.true
-      if !reg.is_a? Nasl::Block
-        report(:error, "The registration section is a #{reg.class.name}, but a Block was expected.")
+      reg = regs.first
+      branch = reg.true
+      unless branch.is_a? Nasl::Block
+        report(:error, "The registration section is a #{branch.class.name}, but a Block was expected.")
+        report(:error, branch.context(reg))
         return fail
       end
 
       # Ensure that the registration section is not empty.
-      if reg.body.empty?
+      if branch.body.empty?
         report(:error, "The registration section is empty.")
+        report(:error, branch.context(reg))
         return fail
       end
 
       # Ensure that the description section ends with a call to exit.
-      last = reg.body.last
-      if !last.is_a?(Nasl::Call)
-        report(:error, "The registration section ends with a #{last.class.name}, not a Pedant::Nasl::Call as expected.")
-        return fail
-      elsif last.name.name != "exit"
-        report(:error, "The registration section ends with a call to #{last.name.name}, not exit as expected.")
+      statement = branch.body.last
+      unless statement.is_a? Nasl::Call
+        report(:error, "The registration section ends with a #{statement.class.name}, not a Call as expected.")
+        report(:error, statement.context(reg))
         return fail
       end
 
-      # Ensure that the call to exit doesn't indicate an error.
-      arg = last.args.first
-      if arg.nil? || arg.type != :anonymous || !arg.expr.is_a?(Nasl::Integer) || arg.expr.value != 0
+      unless statement.name.name == 'exit'
+        report(:error, "The registration section ends with a call to #{statement.name.name}, not exit as expected.")
+        report(:error, statement.context(reg))
+        return fail
+      end
+
+      # Ensure that the call to exit is a success without a message.
+      args = statement.args
+      if args.empty?
+        report(:error, "The registration ends with a call to exit with no arguments.")
+        report(:error, statement.context)
+        return fail
+      end
+
+      arg = args.first
+      if args.length != 1 || arg.type != :anonymous || !arg.expr.is_a?(Nasl::Integer) || arg.expr.value != 0
         report(:error, "The registration section does not end with a call to exit(0).")
+        report(:error, arg.context(statement))
         return fail
       end
     end
