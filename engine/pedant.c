e2f5951c5a1670bde0f0a8a84e96f63fda50e93e
Mak Kolybabi <mak@kolybabi.com>
2013-02-01 16:16:08 -0500

    Executes Lua boot script.

--- engine/pedant.c
+++ engine/pedant.c
@@ -1,6 +1,29 @@
 #include <stdlib.h>
 
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
 int main(int argc, const char **argv, const char **envp)
 {
+	lua_State *L;
+	int rc;
+
+	L = luaL_newstate();
+	if (L == NULL)
+		err(EXIT_FAILURE, "luaL_newstate");
+
+	luaL_openlibs(L);
+
+	rc = luaL_loadfile(L, PKGDATADIR "/boot.lua");
+	if (rc != LUA_OK)
+		err(EXIT_FAILURE, "luaL_loadfile");
+
+	rc = lua_pcall(L, 0, 0, 0);
+	if (rc != LUA_OK)
+		err(EXIT_FAILURE, "lua_pcall");
+
+	lua_close(L);
+
 	return EXIT_SUCCESS;
 }
